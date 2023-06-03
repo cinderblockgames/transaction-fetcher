6471683a57be631b9ec55d5d0144d9d6688f2acb
Kevin A <hello@cinderblockgames.com>
2023-06-03 17:05:42 -0400

    Setting dataDir.

--- src/TransactionFetcher.ActualWrapper/js/api-wrapper.js
+++ src/TransactionFetcher.ActualWrapper/js/api-wrapper.js
@@ -2,7 +2,11 @@ const api = require('@actual-app/api');
 
 async function addTransactions(connectionInfo, accountId, transactions) {
     // Connect and sync up.
-    await api.init({ serverURL: connectionInfo.serverUrl,  password: connectionInfo.serverPassword });
+    await api.init({
+        dataDir: '/data',
+        serverURL: connectionInfo.serverUrl,
+        password: connectionInfo.serverPassword
+    });
     await api.downloadBudget(connectionInfo.budgetSyncId);
 
     // Add the new transactions.
