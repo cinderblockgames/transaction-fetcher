0ffd2ef72947cb3a3e86b1152a90afd81c7b3288
Kevin A <hello@cinderblockgames.com>
2023-06-03 14:50:40 -0400

    Assuming debit for now.

--- src/TransactionFetcher.Readers.TargetRedCard/TargetRedCardTransactionReader.cs
+++ src/TransactionFetcher.Readers.TargetRedCard/TargetRedCardTransactionReader.cs
@@ -46,7 +46,7 @@ public class TargetRedCardTransactionReader : ITransactionReader
             Account = Options!.AccountId,
             Date = message.Date.Date,
             PayeeName = match.Groups["payee"].Value,
-            AmountInCents = (int)(decimal.Parse(match.Groups["amount"].Value, NumberStyles.Currency, Locale) * 100)
+            AmountInCents = -1 * (int)(decimal.Parse(match.Groups["amount"].Value, NumberStyles.Currency, Locale) * 100)
         };
 
         return transaction;
